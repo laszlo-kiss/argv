89394ff92478d87f3644b3b07d04363531bfcb7c
Mogria <m0gr14@gmail.com>
2012-12-14 03:44:42 +0100

    implemented argv_init(), argv_free()

--- src/argv.c
+++ src/argv.c
@@ -1,1 +1,22 @@
 #include <argv/argv.h>
+
+#include <stdlib.h>
+
+struct cmd_args {
+ cmd_option *options;
+};
+
+cmd_args *argv_init(void) {
+  cmd_args *args = calloc(1, sizeof(cmd_args));
+  assert(args != NULL /* calloc() worked? */);
+
+  args->options = NULL;
+
+  return args;
+}
+
+void argv_free(cmd_args *args) {
+  if(args != NULL) {
+    free(args);
+  }
+}
