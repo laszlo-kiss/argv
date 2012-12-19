8116668a77ecccc8ecfe13ea302d7dcfa843f16e
Mogria <m0gr14@gmail.com>
2012-12-19 16:24:29 +0100

    implemented the remaining functions in option.h

--- src/option.c
+++ src/option.c
@@ -5,18 +5,18 @@
 struct cmd_option {
   // parmeter naming
   char shortname;
-  char *longname;
+  const char *longname;
 
   // parameter value
-  char *default_value;
-  char *value;
+  const char *default_value;
+  const char *value;
 
   // parameter type
   int flags;
 
   // parameter info (for --help dialog)
-  char *description;
-  char *value_type_description;
+  const char *description;
+  const char *value_type_description;
 };
 
 static cmd_option *argv_init_option(cmd_args *args) {
@@ -36,11 +36,31 @@ static cmd_option *argv_init_option(cmd_args *args) {
   return option;
 }
 
-cmd_option *argv_option_register(cmd_args *args, char shortname, char *longname, int flags) {
+cmd_option *argv_option_register(cmd_args *args, char shortname, const char *longname, int flags) {
   cmd_option *option = argv_init_option(args);
   assert(shortname != NULL || longname != NULL /* either shortname or an longname given */);
   option->shortname = shortname;
   option->longname = longname;
   option->flags = flags;
   return option;
+}
+
+void argv_option_set_default_value(cmd_option *option, const char *default_value) {
+  option->default_value = default_value;
+}
+
+void argv_option_set_description(cmd_option *option, const char *description) {
+  option->description = description;
+}
+
+void argv_option_set_value_type_description(cmd_option *option, const char *value_type_description) {
+  option->value_type_description = value_type_description;
+}
+
+unsigned char argv_option_given(cmd_option *option) {
+  return value == NULL ? 0 : 1;
+}
+
+const char *argv_option_value(cmd_option *option) {
+  return option->value;
 }
