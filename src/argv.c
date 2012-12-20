c5b6c9c9ceb46ff5a7756e03b88d31253f12934a
Mogria <m0gr14@gmail.com>
2012-12-20 03:11:11 +0100

    added functions to print usage and help text

--- src/argv.c
+++ src/argv.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <argv/argv.h>
 #include "cmd-args.h"
 #include "cmd-option.h"
@@ -120,4 +121,56 @@ int argv_parse_partially(cmd_args *args, const char *programname, int argc, cons
 
 const char **argv_values(cmd_args *args) {
   return args->values;
+}
+
+void argv_usage_print(cmd_args *args) {
+  printf("Usage: %s", args->programname);
+  cmd_option *option;
+  unsigned char required;
+  unsigned char has_value;
+  const char *name;
+  while(NULL != (option = argv_option_iterate(args))) {
+    required = argv_option_value_required(option);
+    has_value = argv_option_needs_value(option);
+    name = option->longname
+    if(name == NULL) {
+      name = &option->shortname;
+    }
+    printf(" %s%s%s%s%s",
+      required ? "" : "[ ",
+      name, has_value ? " " : "",
+      has_value ? option->value_description : "",
+      required ? " ]" : "");
+  }
+}
+
+void argv_help_print(cmd_args *args) {
+  const char *name;
+  unsigned char both;
+  unsigned char is_longname;
+  char dynamic_format[21] = {};
+  char *prefix;
+  printf("Parameters:\n");
+  while(NULL != (option = argv_option_iterate(args))) {
+    name = option->longname;
+    is_longname = 1;
+    both = 0;
+    if(name == NULL) {
+      name = &option->shortname;
+      is_longname = 0;
+    } else if(option->shorname != '\0') {
+      both = 1;
+    }
+
+    if(option->description != NULL)  {
+      if(!both) {
+        snprintf(dynamic_format, 20, "  %s%%-%is", is_longname ? "--" : "-", is_longname ? 19 : 20);
+        printf(dynamic_format, name);
+      } else {
+        snprintf(dynamic_format, 20, "  --%%-%is, -%%c", strlen(option));
+        printf(dynamic_format, option->longname, optoin
+      }
+      printf("%s\n", option->description);
+    }
+  }
 }
